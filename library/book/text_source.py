13efa0511bd248d7677f321b4d69b05ecc70983e
ojara <74782979+odochj@users.noreply.github.com>
2025-12-28 02:12:58 +0000

    wip: enforce entity-aligned project structure

--- library/book/text_source.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Iterator
-
-class TextSource(ABC):
-    @abstractmethod
-    def iter_lines(self) -> Iterator[str]:
-        """Yield text line-by-line, lazily."""
